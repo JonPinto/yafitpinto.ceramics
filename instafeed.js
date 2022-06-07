3d4337934291f1f6dca4122ac61bfe23723b8adb
JonPinto <yonatanpinto1@gmail.com>
2022-06-07 16:32:24 +0300

    seperate file - instafeed script

---
+++ instafeed.js
@@ -0,0 +1,9 @@
+var userFeed = new Instafeed({
+    get: 'user',
+    template: '<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>',
+    limit: 1,
+    target: "instafeed-container",
+    resolution: 'low_resolution',
+    accessToken: 'IGQVJWUDFkZAWlnOHVWVUM4anJQSkVFdThKR1NrZA2VCNW1ac1Q5aU0zWTVfb2xjaWFMWVEtd183ajJtT1VXNE9Lc1B0Yl9xZAHZAOdGZAzVTZAYNlJkcEZAYUlZAFYlFoUkVpSjNLTFNyUVFRaEZAlNjQ3U3dBUgZDZD'
+});
+userFeed.run();
