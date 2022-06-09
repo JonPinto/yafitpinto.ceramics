2123536c5bd73bca6e1db122c802aeb659130d19
JonPinto <yonatanpinto1@gmail.com>
2022-06-09 19:31:40 +0300

    preparing new code, old code wiped

--- instafeed.js
+++ instafeed.js
@@ -1,7 +1,7 @@
 var userFeed = new Instafeed({
     get: 'user',
     template: '<a href="{{link}}"><img title="{{caption}}" src="{{image}}" /></a>',
-    limit: 1,
+    limit: 6,
     target: "instafeed-container",
     resolution: 'low_resolution',
     accessToken: 'IGQVJWUDFkZAWlnOHVWVUM4anJQSkVFdThKR1NrZA2VCNW1ac1Q5aU0zWTVfb2xjaWFMWVEtd183ajJtT1VXNE9Lc1B0Yl9xZAHZAOdGZAzVTZAYNlJkcEZAYUlZAFYlFoUkVpSjNLTFNyUVFRaEZAlNjQ3U3dBUgZDZD'
